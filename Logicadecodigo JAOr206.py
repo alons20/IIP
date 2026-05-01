34b0bda1fadee4813415841c1eb3d07caf136b38
alons20 <alonsoorozco462@gmail.com>
2026-04-30 20:27:54 -0600

    Add files via upload

---
+++ Logicadecodigo JAOr206.py
@@ -0,0 +1,31 @@
+#jose orozco
+
+#nombre de usuario 
+
+nombre = str [input{"cual es tu nombre"}]
+
+print (f"hola mi nombre de usuario es {nombre}")
+
+#cantidad de pizza 
+
+cantidad_de_pizza = int(input{"Cuanta pizza hay"})
+
+print (f"Cuantas rebanadas de pizza hay[cantidad_de_pizza]")
+
+#Esta pagado 
+
+Cantidad_pagada = float("Cuanto dinero se a pagado")
+
+print (f"la cantidad pagada es de {Cantidad_pagada}")
+
+#direccion de envio
+
+Direccion_de_envio = str(input{"Cual es tu direccion"})
+
+print(f"Tu direccion es {Direccion_de_envio}")
+
+#presio total
+
+Presio_total = float(Input{"Cual es el presio total"})
+
+print (f"El presio total es de {Presio_total}")
